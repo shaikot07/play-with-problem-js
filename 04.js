a02dfb92d19b4f96c14eaa436c37e5e285ffce6c
shaikot07 <saifulislamshaikat007@gmail.com>
2024-07-28 21:24:25 +0600

    check object

--- 04.js
+++ 04.js
@@ -4,3 +4,29 @@ let str = "Hello world"
 function reverseString (str) {
     return str.split('').reverse().join('')
 }
+
+////////////////////////
+
+function findAddress(obj) {
+    if (typeof obj !== 'object') {
+          return 'please Provide me a valid Object'
+    } else {
+          const street = obj.street || '__';
+          const house = obj.house || '__';
+          const society = obj.society || '__'
+
+          return street + ',' + house + ',' + society;
+    }
+}
+
+
+
+
+const address = {
+    street: 10,
+    house: '15A',
+    society: 'Earth Perfect'
+}
+
+
+console.log(findAddress(address));
