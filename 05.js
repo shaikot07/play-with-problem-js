0c7689fdcb577f316a8ef4e2989e8f633762a1c1
shaikot07 <saifulislamshaikat007@gmail.com>
2024-07-29 20:10:39 +0600

    check even or  odd

---
+++ 05.js
@@ -0,0 +1,10 @@
+
+// conditional : write a function that check if a number is even or odd;
+
+function checkEvenOrOdd (num) {
+    if(num %2 ==0){
+        return 'Even'
+    }else{
+        return 'Odd'
+    }
+}
