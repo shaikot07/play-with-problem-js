1e60e2f07341abf600745ab3cef57bc20e2b876f
shaikot07 <saifulislamshaikat007@gmail.com>
2024-07-30 23:15:56 +0600

    fixt mistake

--- 07.js
+++ 07.js
@@ -1,14 +1,11 @@
 // Find the sum of all elements in an array;
 
-let arr = [20,30,5,,6,8];
+let arr = [20,30,5,2,6,8];
 let sum = 0;
 
-for(let i=0; i<arr.length;i++){
+
+for(let i=0; i<arr.length; i++){
     sum +=arr[i]
 }
-console.log(sum);
-// for(let i=0; i<arr.length; i++){
-//     sum +=arr[i]
-// }
 
-// console.log(sum);
+console.log(sum);
